7aeb5bccccef001535bc90229a8e3d391373bd50
PAULA BLESA STANIUKAITIS <22282124@CFP127ALUNO.LOCAL>
2023-03-16 11:02:37 -0300

    cepp

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// import { pesquisarCep } from "./viacep.js"
+
+import { pesquisarCep } from "./postmon.js"
+
+
+const preencherFormulario = async() => {
+
+    const cepDigitado = document.getElementById('cep').value
+
+    const cep = await pesquisarCep('06655120')
+
+    document.getElementById('endereco').value = cep.logradouro
+    document.getElementById('bairro').value = cep.bairro
+    document.getElementById('cidade').value = cep.municipio
+    document.getElementById('estado').value = cep.estado
+
+}
+
+document.getElementById('cep').addEventListener('blur', preencherFormulario)
